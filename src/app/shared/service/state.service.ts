2ef11f2a700c2983e751d862e2908d5d21498dc3
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2025-10-20 14:43:52 -0500

    refactor: se arregla la persistencia de la sesión

--- src/app/shared/service/state.service.ts
+++ src/app/shared/service/state.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { StateProps } from '../model/state.enum';
 import { BehaviorSubject, distinctUntilChanged, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+// Debes importar el modelo UserSession aquí
+import { UserSession } from 'src/app/feature/login/model/user-session.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,31 @@ export class StateService {
   
   private stateProps: Map<StateProps, any> = new Map();
   private state = new BehaviorSubject<Map<StateProps, any>>(new Map());
+
+  constructor() {
+    this.rehydrateSession();
+  }
+
+  private rehydrateSession(): void {
+    const token = window.sessionStorage.getItem('Authorization');
+
+    if (token) {
+      try {
+        const tokenPayload = JSON.parse(atob(token.split('.')[1]));
+        const userSession: UserSession = {
+          correo: tokenPayload.email,
+          identificador: tokenPayload.identificador,
+          authorities: tokenPayload.authorities.split(','),
+          logged: true
+        };
+        this.updateState(StateProps.USER_SESSION, userSession);
+
+      } catch (error) {
+        console.error('Error al rehidratar la sesión desde sessionStorage:', error);
+        window.sessionStorage.removeItem('Authorization');
+      }
+    }
+  }
 
   getState(prop:StateProps):any{
     return this.stateProps.get(prop);
@@ -31,5 +58,4 @@ export class StateService {
       distinctUntilChanged() 
     );
   }
-  
 }
