b7f6a40b4d76c59d26f92f7b1ff9d786866af610
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-10-22 00:16:28 -0500

    Se realizan ajustes en la redirección de las rutas al login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeModule } from './feature/home/home.module';
+import { securityGuard } from './core/guard/security.guard';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./feature/login/login.module').then(m => m.LoginModule) },
-{ path: 'home', loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule) },
-{ path: 'gestionar-direccion', loadChildren: () => import('./feature/manage-department/manage-department.module').then(m => m.ManageDepartmentModule) },
-{ path: 'gestionar-usuarios', loadChildren: () => import('./feature/manage-users/manage-users.module').then(m => m.ManageUsersModule) },
-{ path: 'gestionar-indicadores', loadChildren: () => import('./feature/manage-indicators/manage-indicators.module').then(m => m.ManageIndicatorsModule) },
-{ path: 'recuperar-contrasena', loadChildren: () => import('./feature/password-recovery/password-recovery.module').then(m => m.PasswordRecoveryModule) },
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./feature/login/login.module').then(m => m.LoginModule)},
+  { path: 'home', loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule), canActivate: [securityGuard]},
+  { path: 'gestionar-direccion', loadChildren: () => import('./feature/manage-department/manage-department.module').then(m => m.ManageDepartmentModule), canActivate: [securityGuard]},
+  { path: 'gestionar-usuarios', loadChildren: () => import('./feature/manage-users/manage-users.module').then(m => m.ManageUsersModule), canActivate: [securityGuard]},
+  { path: 'gestionar-indicadores', loadChildren: () => import('./feature/manage-indicators/manage-indicators.module').then(m => m.ManageIndicatorsModule), canActivate: [securityGuard]},
+  { path: 'recuperar-contrasena', loadChildren: () => import('./feature/password-recovery/password-recovery.module').then(m => m.PasswordRecoveryModule), canActivate: [securityGuard]},
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
