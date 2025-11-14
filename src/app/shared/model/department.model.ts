5e10c2c8f4360390efcb5bd0e72b6a328515d2a0
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-11-14 13:42:57 -0500

    Se agregan los modelos necesarios para la consulta detallada de las actividades

--- src/app/shared/model/department.model.ts
+++ src/app/shared/model/department.model.ts
@@ -1,4 +1,14 @@
+import { ActivityDetailResponse } from "./activity.model";
+import { AreaDetailResponse } from "./area.model";
+
 export interface DepartmentResponse{
     identificador: string;
     nombre: string;
+}
+
+export interface DepartmentDetailResponse{
+    identificador: string;
+    nombre: string;
+    areas: AreaDetailResponse[];
+    actividades: ActivityDetailResponse[];
 }
