8efbfa94844d9dd3042e688b23be33ebe42444f2
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-11-23 14:56:14 -0500

    Se configura el componente de actividades totales en el modulo de banda sinfónica sub area.

--- src/app/feature/home/modules/bienestar-area/modules/banda-sub-area/components/top-data/top-data.component.ts
+++ src/app/feature/home/modules/bienestar-area/modules/banda-sub-area/components/top-data/top-data.component.ts
@@ -12,4 +12,5 @@ export class TopDataComponent {
   tipoEstructura: 'DIRECCION' | 'AREA' | 'SUBAREA' = 'SUBAREA';
   nombreArea: string = 'Banda Sinfónica';
   imageUrl: string = 'assets/images/Orchestra-amico.png';
+  imageUrl2: string = 'assets/images/Creative team-amico.png';
 }
