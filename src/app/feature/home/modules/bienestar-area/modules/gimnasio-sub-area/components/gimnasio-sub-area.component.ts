3efb9db94ebb1145b6676851c4cb5cd44aa28093
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-11-22 23:53:45 -0500

    Se configura el componente de particpantes totales en el modulo de gimnasio sub area.

--- src/app/feature/home/modules/bienestar-area/modules/gimnasio-sub-area/components/gimnasio-sub-area.component.ts
+++ src/app/feature/home/modules/bienestar-area/modules/gimnasio-sub-area/components/gimnasio-sub-area.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FiltersRequestWithoutArea } from 'src/app/shared/model/filters.model';
 
 @Component({
   selector: 'app-gimnasio-sub-area',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./gimnasio-sub-area.component.scss']
 })
 export class GimnasioSubAreaComponent {
+  filtersRequest: FiltersRequestWithoutArea | null = null;
 
+  onFiltersChanged(filters: FiltersRequestWithoutArea): void {
+    this.filtersRequest = filters;
+  }
 }
