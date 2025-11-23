440e473014198c2e52777ac3fe9147ca3531db14
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-11-22 23:48:41 -0500

    Se configura el componente de participantes totales en el modulo de Extensión sub area

--- src/app/feature/home/modules/bienestar-area/modules/extension-sub-area/components/extension-sub-area.component.ts
+++ src/app/feature/home/modules/bienestar-area/modules/extension-sub-area/components/extension-sub-area.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FiltersRequestWithoutArea } from 'src/app/shared/model/filters.model';
 
 @Component({
   selector: 'app-extension-sub-area',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./extension-sub-area.component.scss']
 })
 export class ExtensionSubAreaComponent {
+  filtersRequest: FiltersRequestWithoutArea | null = null;
 
+  onFiltersChanged(filters: FiltersRequestWithoutArea): void {
+    this.filtersRequest = filters;
+  }
 }
