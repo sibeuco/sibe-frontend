113440cdc180ccfe715b7509e75ea68b670cdbdc
DanielGarQ <danielgarciaquiceno@gmail.com>
2025-11-23 19:55:35 -0500

    Se configura el componente de total-participants-months en los modulos de las áreas.

--- src/app/feature/home/components/botton-data-container/botton-data-container.component.ts
+++ src/app/feature/home/components/botton-data-container/botton-data-container.component.ts
@@ -11,7 +11,7 @@ export class BottonDataContainerComponent {
   
   tipoEstructura: 'DIRECCION' | 'AREA' | 'SUBAREA' = 'DIRECCION';
   nombreArea: string = 'Dirección de Bienestar y Evangelización';
-  participantesColor: string = 'rgba(77, 153, 122, 1)';
-  asistenciasColor: string = 'rgba(4, 181, 172, 1)';
+  participantesColor: string = 'rgba(0, 139, 80, 0.5)';
+  asistenciasColor: string = 'rgba(255, 202, 0, 0.3)';
   title: string = 'Histórico de participación de la Dirección';
 }
