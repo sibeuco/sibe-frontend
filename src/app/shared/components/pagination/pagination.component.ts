396fc4b1ec40b24204e9fcdfd151b85dd9f885a7
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2026-03-23 20:32:57 -0500

    refactor: se ajusta la paginación visualmente

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -2,7 +2,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
-  templateUrl: './pagination.component.html'
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent {
   @Input() paginaActual = 0;
